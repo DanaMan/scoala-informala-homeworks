bb07c2c08bafce6e61eb3944e9aae81aaba91a94
DanaMan <emiliadaniela.man@gmail.com>
2017-08-08 07:14:45 +0300

    Errors fixed

---
+++ java-grupa8/homework-2/src/SortSales.java
@@ -0,0 +1,39 @@
+/**
+ * * Created by Dana Man on 7/30/17.
+ */
+
+/**
+ * Implements a bubble sort algorithm to sort an array of SalesRepresentative objects in descending order.
+ * The sorting should be done based on the amount of revenue generated via sales.
+ * Each rep has a number of sales, and a quota / sale.
+ */
+public class SortSales {
+    public static void sorting(String[] name, int[] number, int[] quota) {
+        int[] revenue = new int[5];
+        for (int a = 0; a < name.length; a++) {
+            revenue[a] = number[a] * quota[a];
+        }
+        int tempValue = 0;
+        String tempName = name[1];
+        int j = 0;
+        while (j < revenue.length - 1) {
+            if (revenue[j] < revenue[j + 1]) {
+                tempValue = revenue[j];
+                revenue[j] = revenue[j + 1];
+                revenue[j + 1] = tempValue;
+                tempName = name[j];
+                name[j] = name[j + 1];
+                name[j + 1] = tempName;
+                j = -1;
+            }
+            j = j + 1;
+        }
+
+        for (int iPrint = 0; iPrint < name.length; iPrint++)
+            System.out.println(name[iPrint] + " " + revenue[iPrint] + "$");
+    }
+
+}
+
+
+
